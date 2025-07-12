8e39b48f067e6503aea076aae6a1a0e894a0506a
bossnabeel <nabeelrahidkhanahmed@gmail.com>
2025-07-13 03:59:07 +0500

    flask

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app=Flask(__name__)
+
 @app.route("/")
 def method_name():
+
     return render_template("index.html")
+if __name__ == "__main__":
+    app.run(debug=True)
